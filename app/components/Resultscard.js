dd631c43361f5373fc53751c44208b9e25ec7dc6
Sherry <lishuxian0738@gmail.com>
2018-06-07 23:10:02 +1000

    hardcode ant design Card component to display on browser

--- app/components/Resultscard.js
+++ app/components/Resultscard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Card } from 'antd';
+const { Meta } = Card;
 
 export default class Resultscard extends React.Component {
 
@@ -8,10 +10,21 @@ export default class Resultscard extends React.Component {
   }
 
   render() {
-
     return <div>
-      <h2>Results and Saved</h2> 
-      </div>
+      <Card title="Results">
+        <Card
+        hoverable
+        style={{ width: 500 }}
+          type="inner"
+          title={<img alt="example" src="http://i4.au.reastatic.net/agencylogo/BFERIC/12/20150619122858.gif" />}
+          cover={<img alt="example" src="http://i1.au.reastatic.net/640x480/88586227f9176f602d5c19cf06261108dbb29f03e30d1c4ce9fc2b51fb1e4bd6/main.jpg" />}  
+        >
+        <Meta
+          title="$560,520"
+          description="www.realestate.com"
+        />
+        </Card>
+    </Card>
+    </div> 
   }
-
 }
