b6479cc3b6f6200be9a6864765b04551ed8fe709
Sherry <lishuxian0738@gmail.com>
2018-06-10 14:41:31 +1000

    update PropertyFunctions.js and get three functions passed

--- test/lib/PropertyFunctions.spec.js
+++ test/lib/PropertyFunctions.spec.js
@@ -14,3 +14,25 @@ test('checkIfPropertyIsSaved should return true if property is saved before.', (
     expect.arrayContaining([expect.objectContaining(property)])
   );
 });
+
+test('savePropertyToSaved should return a new array which contains the property just added in.', () => {
+  const startState = {
+    saved: [{ id: 1 }],
+  };
+  const property = { id: 2 };
+  const result = savePropertyToSaved(property, startState.saved);
+  expect(result).toEqual(
+    expect.arrayContaining([expect.objectContaining(property)])
+  );
+});
+
+test('removePropertyFromSaved should return a new array which does not contain the property just removed.', () => {
+  const startState = {
+    saved: [{ id: 1 }, { id: 2 }, { id: 3 }],
+  };
+  const property = { id: 2 };
+  const result = removePropertyFromSaved(property, startState.saved);
+  expect(result).toEqual(
+    expect.arrayContaining([expect.not.objectContaining(property)])
+  );
+});
